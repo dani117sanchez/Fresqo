50b8d769ffd7d5aba5602bc54b9dbdea97fe1233
dani117sanchez <daniel@creamos.co>
2026-02-28 22:09:42 -0500

    fix: bundle all html files

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
     base: './',
     plugins: [
         tailwindcss(),
     ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                blog: resolve(__dirname, 'blog.html'),
+                recetas: resolve(__dirname, 'recetas.html'),
+            }
+        }
+    }
 })
